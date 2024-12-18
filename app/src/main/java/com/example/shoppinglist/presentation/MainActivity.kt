1fb289f4a19e014ef85e780c0acad125e8dd4ac9
funnyminorudev <funnyminoru@gmail.com>
2024-12-18 15:15:44 +0300

    Added logic for Linear Layout List to MainActivity and replaced ArrayList collection with TreeSet in RepositoryImpl

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -1,27 +1,52 @@
 package com.example.shoppinglist.presentation
 
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
+import com.example.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var llShopList: LinearLayout
 
-    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         viewModel.shopList.observe(this) {
-            Log.d("MainViewModel", it.toString())
-            if (count == 0) {
-                count++
-                viewModel.changeEnableState(it[0])
-            }
+            showList(it)
         }
+    }
+
+    private fun showList(list: List<ShopItem>) {
+        llShopList.removeAllViews()
+        for (shopItem in list) {
+            val layoutId = if (shopItem.enabled) {
+                R.layout.item_shop_enabled
+            } else {
+                R.layout.item_shop_disabled
+            }
+            val view = LayoutInflater.from(this).inflate(layoutId, llShopList, false)
+            val tvName = view.findViewById<TextView>(R.id.tv_name)
+            val tvCount = view.findViewById<TextView>(R.id.tv_count)
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+            view.setOnLongClickListener {
+                viewModel.changeEnableState(shopItem)
+                true
+            }
+            llShopList.addView(view)
+        }
+    }
+
+    private fun initViews() {
+        llShopList = findViewById(R.id.ll_shop_list)
     }
 }
